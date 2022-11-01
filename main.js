425c13a30c851e37507769b1d2461bccb575134b
Marco Vaccaro <97899318+LamaPigro@users.noreply.github.com>
2022-11-01 22:32:52 +0000

    Adding checkbox (! function and marker problems)

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 import './style.css'
 
-// MAP
-var map = L.map('map').setView([59.206000, 18.082900], 13).fitWorld();
+document.title = "markerMap"
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// CHECKBOX
+const nukeBox = document.getElementById("nukeBox")
+const metBox = document.getElementById("metBox")
+const eqBox = document.getElementById("eqBox")
+
+function test() {
+  if (nukebox.checked == true){
+    console.log("hello")
+  } else {
+     console.log("fmmk")
+  }
+}
+
+// MAP
+var map = L.map('map').setView([0,0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
 }).addTo(map);
 
 // ICON
-var nuclearIcon = L.icon({
-    iconUrl: 'nuclearIcon.png',
-    //shadowUrl: 'leaf-shadow.png',
+var nukeIcon = L.icon({
+    iconUrl: 'nukeIcon.png',
+    popupAnchor:  [16, 8] // point from which the popup should open relative to the iconAnchor
+});
 
+var metIcon = L.icon({
+    iconUrl: 'metIcon.png',
     iconSize:     [65, 75], // size of the icon
-    //shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    //shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [10.5, -90] // point from which the popup should open relative to the iconAnchor
+    //popupAnchor:  [10.5, -90] // point from which the popup should open relative to the iconAnchor
 });
 
 // FETCH
@@ -33,7 +48,21 @@ fetch("https://raw.githubusercontent.com/cristianst85/GeoNuclearData/master/data
       const {Latitude, Longitude, Name} = body[i]
       //console.log(Latitude)
       if(Latitude != null || Longitude != null)
-        var markerTest = L.marker([Latitude, Longitude], {icon: nuclearIcon}).addTo(map).bindPopup(Name);
+        var markerNuke = L.marker([Latitude, Longitude], {icon: nukeIcon}).addTo(map).bindPopup(Name);
     }
     
   })
+
+/*fetch("https://data.nasa.gov/resource/gh4g-9sfh.json")
+  .then(r => r.jsx())
+  .then(body => {)
+
+    for (let i = 0; i < body.length; i++) {
+      
+      const {reclat, reclong, name} = body[i]
+      
+      if(reclat != null || reclong != null)
+        var markerNet = L.marker([reclat, reclong], {icon: metIcon}).addTo(map).bindPopup(Name);
+    }
+    
+  })*/
